c94b715e46a2c7e673fbf5e27d8df7401dae4d42
Pablocyc <pablocyc83@gmail.com>
2018-03-13 14:34:51 -0400

    control de 4 luces

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,30 @@
-let boton = document.getElementById("boton");
-let texto = document.getElementById("texto_piso");
-let imgBoton = document.getElementById("img-boton");
+let btn1 = document.getElementById("boton1");
+let btn2 = document.getElementById("boton2");
+let btn3 = document.getElementById("boton3");
+let btn4 = document.getElementById("boton4");
+let imgBoton1 = document.getElementById("img-boton1");
+let imgBoton2 = document.getElementById("img-boton2");
+let imgBoton3 = document.getElementById("img-boton3");
+let imgBoton4 = document.getElementById("img-boton4");
 
-var flag = true;
-boton.addEventListener("click", cambiar);
+var flag1 = true;
+var flag2 = true;
+var flag3 = true;
+var flag4 = true;
 
-function cambiar() {
-	texto.innerHTML = "Dormitorio";
-	if(flag) {
-		imgBoton.src = "images/lampOn.png";
-		flag = false;
-	}
-	else {
-		imgBoton.src = "images/lampOff.png";
-		flag = true;
-	}
+btn1.onclick = function() { 
+	if(flag1) imgBoton1.src = "images/lampOn.png", flag1=false
+	else imgBoton1.src = "images/lampOff.png", flag1=true														
+}
+btn2.onclick = function() { 
+	if(flag2) imgBoton2.src = "images/lampOn.png", flag2=false
+	else imgBoton2.src = "images/lampOff.png", flag2=true														
+}
+btn3.onclick = function() { 
+	if(flag3) imgBoton3.src = "images/lampOn.png", flag3=false
+	else imgBoton3.src = "images/lampOff.png", flag3=true														
+}
+btn4.onclick = function() { 
+	if(flag4) imgBoton4.src = "images/lampOn.png", flag4=false
+	else imgBoton4.src = "images/lampOff.png", flag4=true														
 }
--- server.js
+++ server.js
@@ -2,33 +2,58 @@ const path = require('path')
 const express = require('express');
 const five = require("johnny-five");
 
-
 let app = express();
 let board = new five.Board({port: 'COM4'});
 
 app.use(express.static('public'));
 
-app.get('/on-off', encender);
+app.get('/on-off1', encender1);
+app.get('/on-off2', encender2);
+app.get('/on-off3', encender3);
+app.get('/on-off4', encender4);
 
+board.on("ready", arduino);
 
-function encender(peticion, resultado) {
-	resultado.send(arduino());
+function encender1(peticion, resultado) {
+	resultado.send(arduino(1));
+}
+function encender2(peticion, resultado) {
+	resultado.send(arduino(2));
+}
+function encender3(peticion, resultado) {
+	resultado.send(arduino(3));
+}
+function encender4(peticion, resultado) {
+	resultado.send(arduino(4));
 }
 
+var flagState1 = true;
+var flagState2 = true;
+var flagState3 = true;
+var flagState4 = true;
+
+function arduino(state) {
+	let led1 = new five.Led(8);	
+	let led2 = new five.Led(9);	
+	let led3 = new five.Led(10);	
+	let led4 = new five.Led(11);	
+
+	switch(state) {
+		case 1: if(flagState1) led1.on(), flagState1 = false;
+						else led1.off(), flagState1 = true; break;
+		case 2: if(flagState2) led2.on(), flagState2 = false;
+						else led2.off(), flagState2 = true; break;
+		case 3: if(flagState3) led3.on(), flagState3 = false;
+						else led3.off(), flagState3 = true; break;
+		case 4: if(flagState4) led4.on(), flagState4 = false;
+						else led4.off(), flagState4 = true; break;
+	}
+}
 
 app.listen(3000, function(err) {
 	if(err) return console.log('Hubo un error'), process.exit(1);
 	console.log('Escuchando en el puerto 3000');
 })
-
-var flagState = false;
-board.on("ready", arduino);
-
-function arduino() {
-	let led = new five.Led(13);
-	if(flagState) led.on(), flagState = false;
-	else led.off(), flagState = true;
-}
 
 // board.on("ready", function() {
 // 	let led = new five.Led(13);
@@ -42,3 +67,4 @@ function arduino() {
 				
 // 	})
 // })
+
